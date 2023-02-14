6c49220e6c9bd526ff72dec576bd9919a1f9d670
azeemsajjadlearning <119721824+azeemsajjadlearning@users.noreply.github.com>
2023-02-14 21:33:20 +0530

    prod env correction

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api_prefix: 'https://wah-wah-be.vercel.app/api/v1/',
   token: 'wah-wah-productions',
-  imdb_image_prefix: 'https://image.tmdb.org/t/p/original',
+  imdb_image_prefix: 'https://image.tmdb.org/t/p/',
 };
